b8d5a34290422d4c6d90424ca31940efbff48a2b
Karthik Rao <karthikvrao88@gmail.com>
2018-04-01 12:34:31 +0530

    feat: Create Base view component to hold all other components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BaseView from './components/BaseView';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Initial setup done.
-      </div>
+      <BaseView/>
     );
   }
 }
---
+++ frontend/src/components/BaseView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+
+class BaseView extends Component {
+  render() {
+    return (
+      <div className="base">
+      </div>
+    );
+  }
+}
+
+export default BaseView;
